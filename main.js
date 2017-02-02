d2a06d78f064170b33d1ab190df86840790528be
Brian Hays <brianhays@Brians-MacBook-Pro.local>
2017-02-02 16:40:02 -0700

    JS:String Reporter Exercise

---
+++ main.js
@@ -0,0 +1,3 @@
+var userWord = prompt("Please input a word, any word.")
+
+alert("You entered the word: "+userWord+ "\nThere are " +userWord.length+ " characters in your word.\nLowercase:" +userWord.toLowerCase(userWord)+"\nUppercase:"+userWord.toUpperCase(userWord)+"\nAnd this is how you use "+userWord+" in a sentence.\nSubword:"+userWord.substring(1,4))
